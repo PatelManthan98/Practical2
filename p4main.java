b4a3c4a754208b6ffe4c4da60abf2c4fba461e9c
PatelManthan98 <110323571+PatelManthan98@users.noreply.github.com>
2022-08-06 20:23:57 +0530

    Add files via upload

---
+++ p4main.java
@@ -0,0 +1,13 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class p4main
+{
+    public static void main(String[] args) {
+        p4 a=new p4();
+
+        System.out.println("The average of the no. is:"+a.getaveragee(3));
+        System.out.println("The average of the no. is:"+a.getaverage(11,22));
+        System.out.println("The average of the no. is:"+a.getaverage(32,44,55));
+        System.out.println("Program is done by 21CE098 MANTHAN PATEL");
+    }
+}
